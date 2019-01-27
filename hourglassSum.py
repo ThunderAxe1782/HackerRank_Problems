938cc4231dc28e6c366cc831b3f98c4d722c2c50
Haytham <helgharabawy@hotmail.com>
2019-01-27 17:23:22 +0200

    adding hourglass sum problem solution

---
+++ hourglassSum.py
@@ -0,0 +1,25 @@
+def hourglassSum(arr):
+    max_total = True
+    for i in range(len(arr) - 2):
+        for j in range(len(arr[i]) - 2):
+         total = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+         if max_total == True:
+            max_total = total
+         max_total = max(max_total, total)
+    #print(max_total)
+
+    return max_total
+
+
+
+arr1 = [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]]
+
+arr2 = [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 9, 2, -4, -4, 0], [0, 0, 0, -2, 0, 0], [0, 0, -1, -2, -4, 0]]
+
+arr3 = [[-9, -9, -9, 1, 1, 1], [0, -9, 0, 4, 3, 2], [-9, -9, -9, 1, 2, 3], [0, 0, 8, 6, 6, 0], [0, 0, 0, -2, 0, 0], [0, 0, 1, 2, 4, 0]]
+
+
+
+for a in arr1, arr2, arr3:
+    result = hourglassSum(a)
+    print result
